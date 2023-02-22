c3970db484c5607b02ddfc9d2a2268b3abe6ba93
Fernando Jimenez <fgjv06@gmail.com>
2023-02-22 06:53:18 -0300

    JS - Check for touch screen

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let eyeRef = document.querySelectorAll(".eye");
 
 //Mouse move for devices with mouse and touch move for touch screen devices
 let events = ["mousemove","touchmove"];
+
+//Check for touch screen
+function isTouchDevice() {
+  try{
+    document.createElement("touchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
