ea5645f837346c013ee10d37e07701f66400a800
Fernando Jimenez <fgjv06@gmail.com>
2023-02-22 06:48:54 -0300

    JS - mouse move / touch move

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 // Selecting the eye div
 let eyeRef = document.querySelectorAll(".eye");
+
+//Mouse move for devices with mouse and touch move for touch screen devices
+let events = ["mousemove","touchmove"];
